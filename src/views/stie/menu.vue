cf9778e0c4422eb67493aad0aca22179c975fb62
siha1 <mapelamll@naver.com>
2021-06-19 15:57:15 +0900

    중첩메뉴

--- src/views/stie/menu.vue
+++ src/views/stie/menu.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    <v-list-item v-for="item in items" :key="item.title" :to="item.to">
-      <v-list-item-content>
-        {{ item.title }}
-      </v-list-item-content>
-    </v-list-item>
-  </div>
+  <v-list>
+    <v-list-group
+      v-for="item in items"
+      :key="item.title"
+      v-model="item.active"
+      :prepend-icon="item.action"
+      no-action
+    >
+      <template v-slot:activator>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item-content>
+      </template>
+
+      <v-list-item
+        v-for="subItem in item.subItems"
+        :key="subItem.title"
+        :to="subItem.to"
+      >
+        <v-list-item-content>
+          <v-list-item-title v-text="subItem.title"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
 </template>
 
 <script>
@@ -13,9 +31,26 @@ export default {
   props: ["footer"],
   data: () => ({
     items: [
-      { title: "t1", to: "/" },
-      { title: "t2", to: "/1" },
-      { title: "t3", to: "/2" },
+      {
+        action: "mdi-ticket",
+        subItems: [{ title: "List Item" }],
+        title: "Attractions",
+      },
+      {
+        action: "mdi-silverware-fork-knife",
+        active: true,
+        subItems: [
+          { title: "Breakfast & brunch", to: "/" },
+          { title: "New American", to: "/about" },
+          { title: "Sushi", to: "/" },
+        ],
+        title: "Dining",
+      },
+      {
+        action: "mdi-school",
+        subItems: [{ title: "List Item" }],
+        title: "Education",
+      },
     ],
   }),
 };
